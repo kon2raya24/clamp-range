04100019727a1d5d54184f27cce3c23a0ea5dae6
kon2raya24 <konturaya@gmail.com>
2026-06-24 10:59:37 +0000

    docs: add JSDoc comments to randomint.ts

--- src/randomint.ts
+++ src/randomint.ts
@@ -1,3 +1,8 @@
+/**
+ * randomInt
+ * @param min
+ * @param max
+ */
 export function randomInt(min: number, max: number): number {
   if (min > max) [min, max] = [max, min];
   return Math.floor(Math.random() * (max - min + 1)) + min;
