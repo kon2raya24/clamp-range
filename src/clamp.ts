bb51c759e361a541ff396051e5b2425496f1755b
kon2raya24 <konturaya@gmail.com>
2026-06-24 10:59:41 +0000

    docs: add JSDoc comments to clamp.ts

--- src/clamp.ts
+++ src/clamp.ts
@@ -1,3 +1,9 @@
+/**
+ * clamp
+ * @param value
+ * @param min
+ * @param max
+ */
 export function clamp(value: number, min: number, max: number): number {
   if (min > max) throw new Error("min must be <= max");
   return Math.min(Math.max(value, min), max);
